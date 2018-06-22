6d4fda92d3f961f1956367f0a0aba54e0fc89c54
agrabhi <abhi.iitr@gmail.com>
2018-06-22 10:59:25 -0700

    Add project files.

---
+++ SampleWebApp/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApp
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        static List<Product> products = new List<Product>
+        {
+            new Product() {Id  = "1" }
+        };
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody]Product prod)
+        {
+            products.Add(prod);
+        }
+        
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
