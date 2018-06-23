e9b427574ab2cbca843d5a069b4ee28959307395
agrabhi <abhi.iitr@gmail.com>
2018-06-23 12:20:11 -0700

    d

--- SampleWebApp/Oil.cs
+++ SampleWebApp/Oil.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SampleWebApp
 {
+    [JsonConverter(typeof(ProductJsonConverter))]
     public class Oil: Product
     {
         public int Price { get; set; }
---
+++ SampleWebApp/ProductJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApp
+{
+    public class ProductJsonConverter : JsonCreationConverter<Product>
+    {
+        protected override Product Create(Type objectType, JObject jObject)
+        {
+            //TODO: read the raw JSON object through jObject to identify the type
+            //e.g. here I'm reading a 'typename' property:
+
+            if (jObject["price"] != null)
+            {
+                return new Oil();
+            }
+            return new Product();
+
+            //now the base class' code will populate the returned object.
+        }
+    }
+}
