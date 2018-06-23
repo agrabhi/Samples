1ab3092a3696955c6dc4cb96faa56ee25c7852e9
agrabhi <abhi.iitr@gmail.com>
2018-06-23 13:19:16 -0700

    a

--- SampleWebApp/ProductJsonConverter.cs
+++ SampleWebApp/ProductJsonConverter.cs
@@ -14,13 +14,26 @@ namespace SampleWebApp
             //TODO: read the raw JSON object through jObject to identify the type
             //e.g. here I'm reading a 'typename' property:
 
-            if (jObject["price"] != null)
-            {
+            if (IsTypeOf(jObject, typeof(Oil)))
+            { 
                 return new Oil();
             }
             return new Product();
 
             //now the base class' code will populate the returned object.
+        }
+
+        private bool IsTypeOf(JObject jObject, Type type)
+        {
+            foreach (var prop in type.GetProperties())
+            {
+                if (jObject[prop.Name] == null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
