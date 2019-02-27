67a061ad30c5595c1ccc14e6a56aa7ae7a5c4617
agrabhi <abhi.iitr@gmail.com>
2019-02-27 11:04:04 -0800

    sd

---
+++ SegmentSample/SegmentSample/Models/Trustframework.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SegmentSample.Models
+{
+    public class Trustframework
+    {
+        public string Id;
+
+        public List<TrustframeworkPolicy> Policies;
+
+    }
+}
