a2ea51e17d50788d2e80b392ce8cf5f98618b707
agrabhi <abhi.iitr@gmail.com>
2019-02-27 11:24:20 -0800

    sd

--- SegmentSample/SegmentSample/Models/TrustframeworkPolicy.cs
+++ SegmentSample/SegmentSample/Models/TrustframeworkPolicy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,7 @@ namespace SegmentSample.Models
 {
     public class TrustframeworkPolicy
     {
-        public string Id;
+        public string Id { get; set; }
 
     }
 }
