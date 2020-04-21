66f94e1244322a2ec62dc0ccaffe1d4c60ae87f1
Abhishek Agrawal (CPIM) <abhiagr@microsoft.com>
2020-04-21 10:36:19 -0700

    a

---
+++ IdentityProvidersModelling/WebApplication1/Models/UserProfileAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public abstract class UserProfileAttribute
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public UserProfileAttributeDataType DataType { get; set; }
+    }
+}
---
+++ IdentityProvidersModelling/WebApplication1/Models/UserProfileAttributeDataType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public enum UserProfileAttributeDataType
+    {
+        None = 0,
+
+        String = 1,
+
+        Boolean = 2,
+
+        Int64 = 3,
+
+        Stringcollection = 4,
+
+        unknownFutureValue = 5,
+    }
+}
