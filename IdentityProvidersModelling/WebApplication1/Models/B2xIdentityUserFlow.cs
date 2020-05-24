787c957da0b2c5828a1fbb03c41a3621275602b9
Abhishek Agrawal (CPIM) <abhiagr@microsoft.com>
2020-05-24 01:21:46 -0700

    Done

--- IdentityProvidersModelling/WebApplication1/Models/B2xIdentityUserFlow.cs
+++ IdentityProvidersModelling/WebApplication1/Models/B2xIdentityUserFlow.cs
@@ -4,6 +4,8 @@ namespace WebApplication1.Models
 {
     public class B2xIdentityUserFlow: IdentityUserFlow
     {
+        public UserFlowOtherTenantsSignUpConfiguration OtherTenantsSignUpConfiguration { get; set; }
+
         public ICollection<IdentityProvider> IdentityProviders { get; set; }
 
         public ICollection<UserFlowAttribute> UserAttributes { get; set; }
